1228d122a80e70ebfd5415e30555880b69ac2f7a
Bigaston <git@bigaston.me>
2024-05-17 23:36:37 +0200

    Add Structure Tool

--- objects/placable_elements/Structure.gd
+++ objects/placable_elements/Structure.gd
@@ -1,11 +1,56 @@
+@tool
+
 extends Node3D
 
+var struct_2 = preload("res://resources/meshs/Struct_2.glb")
+var struct_3 = preload("res://resources/meshs/Struct_3.glb")
+var struct_4 = preload("res://resources/meshs/Struct_4.glb")
+const SIZE = 0.6
 
-# Called when the node enters the scene tree for the first time.
+@export_range(2, 4, 1) var number_of_border = 2:
+	set(value):
+		number_of_border = value
+		update_display()
+		
+@export var lenght: int = 1:
+	set(value):
+		if value < 1:
+			value = 1
+			
+		lenght = value
+		update_display()
+
 func _ready():
-	pass # Replace with function body.
+	update_display()
 
+func update_display():
+	if $Container == null:
+		return
+		
+	var obj: PackedScene
+	
+	match number_of_border:
+		2:
+			obj = struct_2
+		3:
+			obj = struct_3
+		4:
+			obj = struct_4
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	for n in $Container.get_children():
+		$Container.remove_child(n)
+		n.queue_free()
+
+	var last_elem: Node3D
+
+	for i in range(lenght):
+		var elem = obj.instantiate()
+		
+		if last_elem:
+			elem.position = last_elem.position + Vector3(0, SIZE, 0)
+		
+		print(elem)
+		
+		$Container.add_child(elem)
+		
+		last_elem = elem
